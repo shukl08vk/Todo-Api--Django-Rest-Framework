10b34a41b5e3d9f153c2b6333357343fcb3ea8f7
Vishnu Kant Shukla <21shukla08@gmail.com>
2021-05-02 23:21:15 +0530

    First commit of Todo api

---
+++ Django Rest Project/todo_drf/api/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from . import views
+
+urlpatterns = [
+    path('', views.apiOverview,name='api-overview'),
+    path('task-create/',views.taskCreate,name='task-create'),
+    path('task-list/',views.taskList,name='task-list'),
+    path('task-detail/<str:pk>',views.taskDetail,name='task-detail'),
+    path('task-update/<str:pk>',views.taskUpdate,name='task-update'),
+    path('task-delete/<str:pk>',views.taskDelete,name='task-delete'),
+]
